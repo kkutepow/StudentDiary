d3a605d56cbea8434e47ca869dea477bf27353a4
Кирилл Кутепов <k.kutepov@ya.ru>
2016-10-03 13:46:05 +0300

    Added comment-stickers to news page

--- app/imports/pages/news/index.js
+++ app/imports/pages/news/index.js
@@ -1,1 +1,22 @@
+import {Template} from "meteor/templating";
 import "./view.html";
+
+Template.news.onCreated(function(){
+    this.boolVar = new ReactiveVar(false);
+    console.log(this.boolVar);
+});
+
+Template.news.helpers({
+   visible : ()=>{
+
+       console.log(Template.instance());
+       return Template.instance().boolVar.get();
+   }
+});
+
+Template.news.events({
+    "click .task-item" : (event, blazeTemplate)=>{
+        let value = blazeTemplate.boolVar.get();
+        blazeTemplate.boolVar.set(!value);
+    }
+});
