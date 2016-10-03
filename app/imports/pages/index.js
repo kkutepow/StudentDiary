44cb867754fccc144cd69fd7b017b78ef57f4a54
Кирилл Кутепов <k.kutepov@ya.ru>
2016-10-03 12:43:51 +0300

    Added simple html views

--- app/imports/pages/index.js
+++ app/imports/pages/index.js
@@ -1,7 +1,9 @@
 
 import "/imports/pages/index.css";
 import "./navbar";
+import "./news";
 import "./schedule";
 import "./group_info";
 import "./achievements";
 import "./taskboard";
+import "./profile";
--- app/imports/startup/client/routes.js
+++ app/imports/startup/client/routes.js
@@ -10,8 +10,8 @@ rootGroup.route("/", {
     name: "root",
     action: (params, queryParams) => {
         console.log("params", params);
-        BlazeLayout.render("login", {
-            userContent: ""
+        BlazeLayout.render("navbar", {
+            userContent: "news"
         });
     }
 });
@@ -20,8 +20,48 @@ rootGroup.route("/schedule", {
     name: "schedule",
     action: (params, queryParams) => {
         console.log("params", params);
-        BlazeLayout.render("login", {
+        BlazeLayout.render("navbar", {
             userContent: "schedule"
+        });
+    }
+});
+
+rootGroup.route("/taskboard", {
+    name: "taskboard",
+    action: (params, queryParams) => {
+        console.log("params", params);
+        BlazeLayout.render("navbar", {
+            userContent: "taskboard"
+        });
+    }
+});
+
+rootGroup.route("/group_info", {
+    name: "group_info",
+    action: (params, queryParams) => {
+        console.log("params", params);
+        BlazeLayout.render("navbar", {
+            userContent: "group_info"
+        });
+    }
+});
+
+rootGroup.route("/achievements", {
+    name: "achievements",
+    action: (params, queryParams) => {
+        console.log("params", params);
+        BlazeLayout.render("navbar", {
+            userContent: "achievements"
+        });
+    }
+});
+
+rootGroup.route("/profile", {
+    name: "profile",
+    action: (params, queryParams) => {
+        console.log("params", params);
+        BlazeLayout.render("navbar", {
+            userContent: "profile"
         });
     }
 });
